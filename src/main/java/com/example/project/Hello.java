d03ed6f6ace1ebbdda411e7a7685fcbc7d51913a
hyechxn <a49918072@gmail.com>
2025-03-11 10:13:48 +0900

    commit

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -3,8 +3,9 @@ package com.example.project;
 public class Hello {
 
 	public static void main(final String[] args) {
-		System.out.println("Not the right string, tests will fail!");
+		System.out.println("Hyechan");
 	}
+
 
 }
 
